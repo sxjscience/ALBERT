6b95601419cd463706fa3d4e2d155f74ac4809fb
Xingjian Shi <xshiab@connect.ust.hk>
2019-12-12 13:04:44 -0800

    Update download_pretrained_models.sh

--- download_pretrained_models.sh
+++ download_pretrained_models.sh
@@ -4,5 +4,7 @@ for model in base large xlarge xxlarge
 do
   mkdir albert_${model}_v2
   wget https://tfhub.dev/google/albert_base/2?tf-hub-format=compressed -O albert_${model}_v2/albert_${model}_v2.tar.gz
-  tar -xvf albert_${model}_v2/albert_${model}_v2.tar.gz
+  cd albert_${model}_v2
+  tar -xvf albert_${model}_v2.tar.gz
+  cd ..
 done
