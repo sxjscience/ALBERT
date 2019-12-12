1c106fce2a2a26a0588af5b3acdc20e14dc8ba13
Xingjian Shi <xshiab@connect.ust.hk>
2019-12-12 13:02:00 -0800

    Create download_pretrained_models.sh

---
+++ download_pretrained_models.sh
@@ -0,0 +1,8 @@
+set -x
+
+for model in [ base large xlarge xxlarge]
+do
+  mkdir albert_${model}_v2
+  wget https://tfhub.dev/google/albert_base/2?tf-hub-format=compressed -O albert_${model}_v2/albert_${model}_v2.tar.gz
+  tar -xvf albert_${model}_v2/albert_${model}_v2.tar.gz
+done
